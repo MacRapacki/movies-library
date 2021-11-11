7062f1119810cba734348a4a73e43cb01d696702
Maciej Rapacki <mac.rapacki@gmail.com>
2021-11-11 14:52:30 +0000

    fix

--- src/components/useFavoriteMovieCard.js
+++ src/components/useFavoriteMovieCard.js
@@ -1,6 +1,3 @@
-import { useDispatch } from "react-redux";
-import { addToMyLibrary } from "../store/myLibrarySlice";
-
 const useFavoriteMovieCard = () => {
   const ratings = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
